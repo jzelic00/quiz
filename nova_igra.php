78690e72b78eea60be41005ea039c98a5cce9149
projectAntonov7000 <32770399+projectAntonov7000@users.noreply.github.com>
2017-11-11 17:17:10 +0100

    Update nova_igra.php

--- nova_igra.php
+++ nova_igra.php
@@ -1,29 +1,25 @@
 <?php
-
 	// Ako zelimo mijenjati broj pitanja koje zelimo imati u igri, moramo promjeniti neke stvari
 	// U PHP DIJELU TREBA PROMJENITI SLJEDECE:
 	// 		- broj elemenata niza nizPitanja (broj elemenata = broju pitanja koliko zelimo po igri)
 	//		+ broj elemenata niza nizTocnihOdgovora ( broj elemenata = broj elemenata niza nizPitanja)
 	//		- vrijednost varijable brojPitanja (ista broju elemenata gornjih nizova)
 	// U JAVASCRIPT DIJELU TREBA PROMIJENITI SLJEDECE:
-	//  	+ broj elemenata niza nizTacnihOdgovora treba imati isti broj elemenata kao nizovi u PHP dijelu (nizPitanja i nizTocnihOdgovora)
 	//		+ broj elemenata niza nizOdgovora treba imati isti broj elemenata kao nizTacnihOdgovora
-	//		- ucitati dodatne tocne odgovore u nizTacnihOdgovora
 	//		+ vrijednost do koje brojac "i" treba ici (onoliko koliko ima pitanja, tj. vrijednost varijable brojPitanja)
 	//		+ promjeniti uvjet if grananja (i treba provjeravat je li isti kao brojPitanja - 1) 
 	//------------------------------------------- 
-
 	$dept = array();
 	$nizIskoristenih = array(); // Broj elemenata ovog niza ovisi o tome koliko cemo odgovora ponuditi korisniku na izbor (fixno 4)
 	$nizPitanja = array(0,0,0,0); // Broj elemenata niza ovisi o tome koliko pitanja zelimo staviti po igri
-	$nizTocnihOdgovora = array(); // Broj elemenata ovog niza mora biti jednak broju elemenata niza nizPitanja, jer za svako pitanje 										  // moramo imati tocan odgovor
+	$nizTocnihOdgovora = array(); // Broj elemenata ovog niza mora biti jednak broju elemenata niza nizPitanja, jer za svako pitanje 										 // moramo imati tocan odgovor
 	$indexPitanja=0; // Varijabla pomocu koje cemo prolaziti kroz nizPitanja kako bismo iskoristili random generirane ID-eve
 	$brojOdgovora=4; // Varijabla koja oznacava koliko ce korisniku biti ponudeno odgovora (u ovom slucaju, kako nudimo samo a,b,c,d opciju)
 					 // ova varijabla je uvijek 4, neovisno o broju pitanja; broj je jednak broju elemenata niza nizIskoristenih
-	$brojPitanja=4;  // Varijabla koj oznacava koliko ce biti pitanja po igri
+	$brojPitanja=4; // Varijabla koj oznacava koliko ce biti pitanja po igri
 	$indexTocnogOdgovora=0; // Varijabla pomocu koje cemo prolaziti kroz nizTocnihOdgovora kako bismo iskoristili random generirane brojeve
 							// za raspored odgovora koji ce biti ponudeni korisniku
-
+	//------------------------------------------- 
 	// U nizIskoristenih cemo spremiti 4 broja
 	// Ta 4 broja ce biti u rangeu od 1 do 4
 	// To nam sluzi kako bismo izmjesali odgovore u odnosu na njihov polozaj u bazi podataka
@@ -36,13 +32,10 @@
 		$brojac2 = 0;
 		$flag = false;
 		$rndBroj = 0;
-
 		// Postavljanje svih clanova niza na vrijednost 0 
 		for($z=0; $z<$brojOdgovora; $z++){
 			$nizIskoristenih[$z] = 0;
 		}
-
-
 		// Algoritam za generiranje niza random brojeva u odredenom rasponu
 		// Posto znamo da cemo korisniku na izbor ponuditi 4 odgovora, radimo glavnu petlju koja ce se provrtiti 4 puta
 		// Brojac pocinje od 0, sto znaci da ce imati vrijednosti 0,1,2,3 sto ce ujedno biti i indexi na koje cemo spremati 
@@ -56,7 +49,6 @@
 		while($brojac2 < $brojOdgovora){
 			$flag = true;
 			$rndBroj = rand(1,4);
-
 			for($j=0; $j<4; $j++){
 				if($nizIskoristenih[$j] == $rndBroj){
 					$flag = false;
@@ -110,7 +102,6 @@
 		global $connect;
 		global $nizTocnihOdgovora;
 		global $indexTocnogOdgovora;
-
 		// Spajanje na bazu, sa sql upitom, fohvacamo pitanje sa ID-em kojeg smo random generirali
 		// Citav output se sprema u red kao string, pa tako moramo iz varijable $red izvuci odgovore i spremiti u odvojene varijable
 		//------------------------------------------- 
@@ -137,37 +128,60 @@
 		$odgovor4 = $dept[$nizIskoristenih[3]];
 		$indexPitanja++;
 	}
-
 ?>
 
 <form id="form1" action="index.php?action=nova_igra" method="POST">	
 				<?php
-					for($i=0; $i< $brojPitanja; $i++)
+					for($i=0; $i< $brojOdgovora; $i++)
 					{
 						$b=$i+1;
+						global $imePitanja;
 						PostaviPitanje();
 							echo '
 							<div id="div'.$i.'">
-									<div class="naslov">
+							<div id="obican_box2">
+								<div id="obican_box_naslov2">
+									Pitanje
+									'.$b.'/'.$brojOdgovora.'		
+								</div>
+								<div class="naslov">
 										 '. $red["Pitanje"] . '
 									</div>
 									<div class="gumbici">
-										<button type="button" class="butt_new" value="' . $odgovor1 . '" onclick="myFunction(value)"> '. $odgovor1 . '</button><br>
-										<button type="button" class="butt_new" value="' . $odgovor2 . '" onclick="myFunction(value)"> '. $odgovor2 . '</button><br>
-										<button type="button" class="butt_new" value="' . $odgovor3 . '" onclick="myFunction(value)"> '. $odgovor3 . '</button><br>
-										<button type="button" class="butt_new" value="' . $odgovor4 . '" onclick="myFunction(value)"> '. $odgovor4 . '</button><br>
+										<button type="button" class="butt_new" name="'.$imePitanja. '" value="' . $odgovor1 . '" onclick="myFunction(value)"> '. $odgovor1 . '</button><br>
+										<button type="button" class="butt_new" name="'.$imePitanja. '" value="' . $odgovor2 . '" onclick="myFunction(value)"> '. $odgovor2 . '</button><br>
+										<button type="button" class="butt_new" name="'.$imePitanja. '" value="' . $odgovor3 . '" onclick="myFunction(value)"> '. $odgovor3 . '</button><br>
+										<button type="button" class="butt_new" name="'.$imePitanja. '" value="' . $odgovor4 . '" onclick="myFunction(value)"> '. $odgovor4 . '</button><br>
 									</div>
+							</div>
+									
 							</div>';								
+						$imePitanja++;
 					};
-					$_SESSION['varname'] = $nizTocnihOdgovora;					
+					$_SESSION['nizTocnihOdgovora'] = $nizTocnihOdgovora;					
 				?>
+<div id="div4">
+	<div id="obican_box2">
+		<div id="obican_box_naslov2">
+			<p>ZAVRŠENA IGRA</p>	
+		</div>
+		<div class="naslov">
+				<p> Ostvareni rezultat:</p>
+		</div>
+		<div class="gumbici">
+			<p>BROJ BODOVA: <span id="ostvarniBod"></span></p>
+			<p>BROJ TOCNIH ODGOVORA: <span id="tocniOdgovori"></span></p>
+		</div>
+	</div>	
+</div>	
+
 <script>
 	var nizOdgovora = [];
-	var nizTacnihOdgovora = [];
 	var i = 0;
 	var bodovi = 0;
 	var brPitanja = "<?php echo $brojPitanja ?>";
-
+	var vraceno="";
+	var ss = [];
 	// U nizTacnihOdgovora spremamo sve tocne odgovore koje smo povukli iz baze koje cemo kasnije iskoristiti za usporedbu
 	// 		sa unesenim odgovorima korisnika (korisnikove odgovore spremamo u nizOdgovora)
 	// Svaki put kada se klikne neki odgovor, poziva se ova funkcija i sprema kliknutu vrijednost u nizTacnihOdgovora
@@ -178,30 +192,26 @@
 	function myFunction(vrijednost) {
 		nizOdgovora[i] = vrijednost;
 		if(i==(brPitanja - 1)) {
-
 			var xmlhttp = new XMLHttpRequest();
        		xmlhttp.onreadystatechange = function() {
 	            if (this.readyState == 4 && this.status == 200) {
-	            	console.log('Uslo u if');
-	            	console.log('varijabla is PHP: '+this.responseText);
-	                //document.getElementById("txtHint").innerHTML = this.responseText;
+	            	//bodovi=this.responseText;
+
+	            	//window.alert(this.responseText);
+	            	vraceno = this.responseText;
+	            	ss = vraceno.split(",");
+
 	            }
         	};
         	xmlhttp.open("GET", "calculate.php?korisnik="+nizOdgovora, false);
        		xmlhttp.send();
-
-			/*nizTacnihOdgovora[0] = "<?php echo $nizTocnihOdgovora[0] ?>";
-			nizTacnihOdgovora[1] = "<?php echo $nizTocnihOdgovora[1] ?>";
-			nizTacnihOdgovora[2] = "<?php echo $nizTocnihOdgovora[2] ?>";
-			nizTacnihOdgovora[3] = "<?php echo $nizTocnihOdgovora[3] ?>";
-			for(i=0;i<brPitanja;i++){
-				if(nizOdgovora[i]==nizTacnihOdgovora[i])
-					bodovi += 25;
-			}*/
-			//console.log(nizOdgovora);
-			//window.alert("Ukupno bodova: " + bodovi);
-			window.alert("IGRA ZAVRSENA!");
-			window.location.href = "index.php";
+       		document.getElementById('ostvarniBod').innerHTML = ss[0];
+       		document.getElementById('tocniOdgovori').innerHTML = ss[1];
+		
+			document.getElementById('div'+i).style.display = "none";
+			i++;
+			document.getElementById('div'+i).style.display = "block";
+			setTimeout(function(){ window.location.href = "index.php"; }, 3000);
 		}
 		else {
 			document.getElementById('div'+i).style.display = "none";
