dddc84acf9e658ee1df354dc98be4dd40888f416
projectAntonov7000 <32770399+projectAntonov7000@users.noreply.github.com>
2017-11-11 17:17:30 +0100

    Update calculate.php

--- calculate.php
+++ calculate.php
@@ -1,47 +1,41 @@
 <?php 
 	session_start();
 	include('config.php');
-
-
-
 	$korOdgovori = $_REQUEST["korisnik"];
-	$myArray = explode(',', $korOdgovori);
-	$var_value = $_SESSION['varname'];
-	$username = $_SESSION['login_username'];
+	$nizKorisnikovihOdgovora = explode(',', $korOdgovori);
+	$nizTocnihOdgovora = $_SESSION['nizTocnihOdgovora'];
 	$id = $_SESSION['ID']; 
-
 	$i=0;
 	$bodovi=0;
+	$vrati = 0;
+	$tocni=0;
 
 	for($i=0;$i<4;$i++){
-		if($myArray[$i]==$var_value[$i]){
-			$bodovi = $bodovi + 25;
+		if($nizKorisnikovihOdgovora[$i]==$nizTocnihOdgovora[$i]){
+			$bodovi += 10;
+			$tocni++;
+			continue;
 		}
+		$bodovi -= 15;
 	}
+	
 
-	/*
-	$upit ="SELECT Bodovi as Bodovi FROM quiz_user WHERE ID=1";
+	$upit ="SELECT Bodovi as Bodovi FROM quiz_user WHERE ID=$id";
 	$db_output = mysqli_query($connect, $upit);
 	$count = $db_output->fetch_assoc();
 
+	if($bodovi<=0){
+		$bodovi = 0;
+	}
+
+	$vrati = $bodovi;
+	$vrati = $vrati . "," . $tocni;
+
 	$bodovi += $count['Bodovi'];
-
-	*/
+	
 	$sql = "UPDATE quiz_user SET Bodovi=$bodovi WHERE ID=$id";
+	mysqli_query($connect, $sql);
 
-	if (mysqli_query($connect, $sql)) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . mysqli_error($connect);
-}
-	//$db_output = mysqli_query($connect, $sql);
 	//mysqli_close($conn);
-
-	echo $bodovi;
-
+	echo $vrati;
 ?>
-
-
-	
-
-	
